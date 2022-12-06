c0fb315f87ccc9c105ead1328f46879b21ca9e2c
natfedd <ryabikhina@mail.ru>
2022-12-06 16:55:21 +0500

    06.12.

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,37 @@ public class Main {
     public static void main(String[] args) {
         // Домашнее задание 1. Задача 1
         System.out.println("Домашнее задание 1. Задача 1");
-        int[] numbers = new int[3];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
-        double[] numbers2 = {1.57, 7.654, 9.986};
-        int[] numbers3 = {8, 5, 9, 65, 3};
-        // Домашнее задание 1. Задача 2
-        System.out.println("Домашнее задание 1. Задача 2");
-        int[] numbers4 = new int[3];
-        numbers4[0] = 1;
-        numbers4[1] = 2;
-        numbers4[2] = 3;
-        double[] numbers5 = {1.57, 7.654, 9.986};
-        int[] numbers6 = {8, 5, 9, 65, 3};
-        System.out.println(numbers4[0] + "," + numbers4[1] + "," + numbers4[2]);
-        System.out.println(+numbers5[0] + "," + numbers5[1] + "," + numbers5[2]);
-        System.out.println(+numbers6[0] + "," + numbers6[1] + "," + numbers6[2] + "," + numbers6[3] + "," + numbers6[4]);
-        // Домашнее задание 1. Задача 3
-        System.out.println("Домашнее задание 1. Задача 3");
-        System.out.println(numbers4[2] + "," + numbers4[1] + "," + numbers4[0]);
-        System.out.println(+numbers5[2] + "," + numbers5[1] + "," + numbers5[0]);
-        System.out.println(+numbers6[4] + "," + numbers6[3] + "," + numbers6[2] + "," + numbers6[1] + "," + numbers6[0]);
-        // Домашнее задание 1. Задача 4
-        System.out.println("Домашнее задание 1. Задача 4");
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 != 0) {
-                numbers[i] = numbers[i] + 1;
+        int [] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        int [] weightsCopy = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+
+        }
+        boolean arraysAreEqual = true;
+        arraysAreEqual = weights.length == weightsCopy.length;
+        if (arraysAreEqual) {
+            for (int i = 0; i < weights.length; i++) {
+                if (weights[i] !=weightsCopy[i]) {
+                    arraysAreEqual = false;
+                }
+                
             }
-            System.out.println(numbers[i]);
+        }
+        if (arraysAreEqual) {
+            System.out.println("Массивы одинаковые");
+        }
+        else {
+            System.out.println("Массивы разные");
+        }
+        for (int i = 0; i < weightsCopy.length; i++) {
+            System.out.println(weightsCopy[i]);
+            
         }
     }
 }
