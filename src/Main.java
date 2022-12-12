45fdf5e31ca21404810a4e1e7bbade95bdba85d4
natfedd <ryabikhina@mail.ru>
2022-12-12 11:17:17 +0500

    12.12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var firstBoxerWeights = 78.2;
+        var secondBoxerWeights = 82.7;
+        var totalWeights = firstBoxerWeights + secondBoxerWeights;
+        System.out.println("Общий вес боксеров составляет " + totalWeights + "кг.");
+        var weightDifference = secondBoxerWeights - firstBoxerWeights;
+        System.out.println("Разница в весе " + weightDifference + "кг.");
+        weightDifference = secondBoxerWeights % firstBoxerWeights;
+        System.out.println("Разница в весе (другим способом) " + weightDifference + "кг.");
+        var totalWorkTime = 640;
+        var workTime = 8;
+        var staff = totalWorkTime / workTime;
+        System.out.println("Всего работников в компании – " + staff + " человек");
+        staff = staff + 94;
+        totalWorkTime = staff * workTime;
+        System.out.println("Если в компании работает " + staff + " человек, то всего " + totalWorkTime + " часов работы может быть поделено между сотрудниками");
+
+
+
+
     }
 }
