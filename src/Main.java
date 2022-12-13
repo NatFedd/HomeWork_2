7da0551f7422c2fb0a6b3d37757b8c95ded86a23
natfedd <ryabikhina@mail.ru>
2022-12-13 16:49:20 +0500

    12.12

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,28 @@
 public class Main {
+    public static void printYear(int[] year) {
+
+    }
+
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog - 3.5;
-        cat = cat - 1.6;
-        paper = paper - 7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        var friend = 19;
-        System.out.println(friend);
-        friend = friend * 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
-        var firstBoxerWeights = 78.2;
-        var secondBoxerWeights = 82.7;
-        var totalWeights = firstBoxerWeights + secondBoxerWeights;
-        System.out.println("Общий вес боксеров составляет " + totalWeights + "кг.");
-        var weightDifference = secondBoxerWeights - firstBoxerWeights;
-        System.out.println("Разница в весе " + weightDifference + "кг.");
-        weightDifference = secondBoxerWeights % firstBoxerWeights;
-        System.out.println("Разница в весе (другим способом) " + weightDifference + "кг.");
-        var totalWorkTime = 640;
-        var workTime = 8;
-        var staff = totalWorkTime / workTime;
-        System.out.println("Всего работников в компании – " + staff + " человек");
-        staff = staff + 94;
-        totalWorkTime = staff * workTime;
-        System.out.println("Если в компании работает " + staff + " человек, то всего " + totalWorkTime + " часов работы может быть поделено между сотрудниками");
+        //Реализуйте метод, который получает в качестве параметра год, а затем проверяет,
+        // является ли он високосным, и выводит результат в консоль.
+        //Теперь проверку нужно обернуть в метод и использовать год,
+        // который приходит в виде параметра.
+        //Результат программы выведите в консоль.
+        // Если год високосный, то должно быть выведено “*номер года
+        // —* високосный год”. Если год не високосный,
+        // то, соответственно: “*номер года —* не **високосный год”.
+        //Задача 1
+        System.out.println("Задача 1");
 
-
+        //int year = 1803;
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год - висосный");
+        } else {
+            System.out.println(year + " - не висосный год");
+        }
 
 
     }
-}
+
+    }
